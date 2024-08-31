d3a095de9aaf4ed1c028d8106f5c612b2f2183c7
devopsboi <abhirajpalyekar@gmail.com>
2024-08-31 18:11:10 +0530

    Configured Postgres Database - 1.

--- app/app/test.py
+++ app/app/test.py
@@ -2,7 +2,7 @@
 Sample tests
 """
 
-from django.test import SimpleTestCase # type: ignore
+from django.test import SimpleTestCase  # type: ignore
 
 from app import calc
 
@@ -12,12 +12,12 @@ class CalcTests(SimpleTestCase):
 
     def test_add_numbers(self):
         """Test adding number together."""
-        res = calc.add(5,5)
+        res = calc.add(5, 5)
 
         self.assertEqual(res, 10)
 
     def test_sub_numbers(self):
         """Test subtracting number together"""
-        res = calc.subtract(5,3)
+        res = calc.subtract(5, 3)
 
         self.assertEqual(res, 2)
